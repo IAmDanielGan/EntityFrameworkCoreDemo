c64d0305cd209dc2171cf4fc6ad077b3c9b4cb63
IAmDanielGan <ganyuansong@gmail.com>
2023-04-09 00:49:18 -0700

    move database connect string to appsetting.json: The command script is following: Scaffold-DbContext -Connection Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models -force

--- BookStoreWebAPI/Models/BookStoresDbContext.cs
+++ BookStoreWebAPI/Models/BookStoresDbContext.cs
@@ -36,8 +36,7 @@ public partial class BookStoresDbContext : DbContext
     public virtual DbSet<User> Users { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=(localdb)\\ProjectModels;Initial Catalog=BookStoresDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        => optionsBuilder.UseSqlServer("Name=BookStoresDB");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
